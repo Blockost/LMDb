8c9ebdeadf41d7462408cac1ed3e8e61da8fc6bb
Simon Espigolé <espigolesi@eisti.eu>
2017-01-30 16:57:26 +0000

    Now query each movie id individually in the DB until /movie/latest's id is reached

--- scrapper.js
+++ scrapper.js
@@ -6,8 +6,9 @@
 let request = require('request');
 let mysql = require("mysql");
 
-const API_KEY_V3 = 'YOUR_API_KEY';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY_V3 = 'f1b8e21ba67985a66c2ef448f467e3c7';
+const BASE_URL = 'https://api.themoviedb.org/3/movie/120621';
+const LATEST_MOVIE_ID = 438012; // 29/01/17
 let params;
 
 let con = mysql.createConnection({
@@ -17,20 +18,10 @@ let con = mysql.createConnection({
     database: 'lmdb'
 });
 
-/*
- * Tweak these options to get different types of movies
- * Because the API allows to crawl to page 1000 only,
- * be smart and create useful queries :D
- */
-
 let options = {
     method: 'GET',
-    url: BASE_URL + '/discover/movie',
+    url: BASE_URL,
     qs: {
-        include_video: 'false',
-        include_adult: 'false',
-        'vote_count.lte': '100',
-        sort_by: 'vote_count.desc',
         language: 'en-US',
         api_key: API_KEY_V3
     },
@@ -39,28 +30,35 @@ let options = {
 
 /*
  * Main loop
- * Iterates over 1000 pages of results
- * May be more than 20000 results but this is the maximum allowed by the API... :'(
+ * Iterated over every ID in the database
+ * From 1 to /movie/latest
  */
 
 function sendRequest(options) {
-    console.log("current page: " + options.qs.page);
-    if (options.qs.page <= 1000) {
+    let current_movie_id = parseInt(getMovieIdFromUrl(options.url));
+    if (current_movie_id <= LATEST_MOVIE_ID) {
         request(options, function (err, response, body) {
+            let next_movie_id = current_movie_id + 1;
+
             if (err) console.log(err);
 
             // If requests get rejected because of API overflowed...
             if (response.statusCode == 429) {
                 console.log('Too much requests');
                 setTimeout(() => {
-                    options.qs.page++;
+                    options.url = incrementUrl(options.url, next_movie_id);
                     sendRequest(options);
                 }, 10000);
 
+                // If movie not found
+            } else if (response.statusCode == 404) {
+                // Perhaps moved or deleted
+                options.url = incrementUrl(options.url, next_movie_id);
+                sendRequest(options);
             } else {
                 // Pass it to the response handler
-                responseHandler(response, body);
-                options.qs.page++;
+                responseHandler(body);
+                options.url = incrementUrl(options.url, next_movie_id);
                 sendRequest(options);
             }
         });
@@ -68,45 +66,50 @@ function sendRequest(options) {
 }
 
 
-function responseHandler(response, body) {
+function responseHandler(body) {
 
-    if (response.statusCode == 200) {
-        let jsonResponse = JSON.parse(body);
-        let numberOfResults = jsonResponse.results.length;
+    let movie = JSON.parse(body);
 
-        // For every movie retrieved...
-        for (let i = 0; i < numberOfResults; i++) {
+    // If the movie is admissible, e.g has a short plot
+    if (isMovieAdmissible(movie)) {
+        params = [
+            movie.id,
+            movie.title,
+            movie.overview,
+            movie.vote_average,
+            movie.vote_count
+            // + movie.tagline for experiment purposes ?
+        ];
 
-            let movie = jsonResponse.results[i];
-            // If the movie is admissible, e.g has a short plot
-            if (isMovieAdmissible(movie)) {
-                params = [
-                    movie.id,
-                    movie.title,
-                    movie.overview,
-                    movie.vote_average,
-                    movie.vote_count
-                ];
-
-                // Add it to the database
-                con.query('INSERT INTO movies SET ' +
-                    'TMDbId = ?, title = ?, shortPlot = ?, rating = ?, numberOfVotes = ?',
-                    params,
-                    (err, result) => {
-                        // Potential common error is DUPLICATE_FOUND
-                        if (err) console.log(err.code + ' for ' + movie.title);
-                    });
-            }
-        }
-
+        // Add it to the database
+        con.query('INSERT INTO movies SET ' +
+            'TMDbId = ?, title = ?, shortPlot = ?, rating = ?, numberOfVotes = ?',
+            params,
+            (err, result) => {
+                // Potential common error is DUPLICATE_FOUND
+                if (err) console.log(err.code + ' for ' + movie.title + ' (' + movie.id + ')');
+            });
     }
 }
 
 function isMovieAdmissible(movie) {
-    return movie.overview.split(' ').length >= 10;
+    // English
+    // not adult
+    return !movie.adult /*&& movie.overview.split(' ').length >= 10*/;
+}
+
+function getMovieIdFromUrl(url) {
+    let split_url = url.split('/');
+    let length = split_url.length;
+    return split_url[length - 1]
+}
+
+function incrementUrl(url, nextMovieId) {
+    let split_url = url.split('/');
+    let length = split_url.length;
+    split_url[length - 1] = nextMovieId;
+    return split_url.join('/');
 }
 
 
-// Start at page one and increment page index afterwards...
-options.qs.page = 1;
 sendRequest(options);
